2c43d814892fcb5889030e98ba5c5b115ad8cc8b
saleh <saleh.mk96@gmail.com>
2021-04-26 11:34:59 -0500

    Added API Get Calls.

--- app/src/main/java/comsaleh/caloriesapp/CaloriesRunnable.java
+++ app/src/main/java/comsaleh/caloriesapp/CaloriesRunnable.java
@@ -1,9 +1,19 @@
 package comsaleh.caloriesapp;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class CaloriesRunnable implements Runnable {
 
     private MainActivity mainActivity;
-    private static final String url = "https://nutritionix-api.p.rapidapi.com/v1_1/";
+    private static final String TAG = "CaloriesRunnable";
+    private static final String URL = "https://nutritionix-api.p.rapidapi.com/v1_1/search/";
     private static final String API_KEY = "8694c31524msh9489d792de20f42p137d32jsn7a3cd585ce55";
     private static final String HOST = "nutritionix-api.p.rapidapi.com";
     private String query;
@@ -16,5 +26,44 @@ public class CaloriesRunnable implements Runnable {
     @Override
     public void run() {
 
+        Uri.Builder builder = Uri.parse(URL+query).buildUpon();
+        builder.appendQueryParameter("fields","item_name,item_id,brand_name,nf_calories,nf_total_fat");
+        String urlString = builder.toString();
+        Log.d(TAG, "run: "+urlString);
+        StringBuilder strBuilder = new StringBuilder();
+        try{
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("x-rapidapi-key",API_KEY);
+            connection.setRequestProperty("x-rapidapi-host",HOST);
+            connection.connect();
+            if(connection.getResponseCode() != HttpURLConnection.HTTP_OK)
+            {
+                StringBuilder result = new StringBuilder();
+                BufferedReader reader = null;
+                Log.d(TAG, "run: "+connection.getResponseCode());
+                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+
+                String line;
+                while (null != (line = reader.readLine())) {
+                    result.append(line).append("\n");
+                }
+                Log.d(TAG, "run: "+result.toString());
+                return;
+            }
+
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String data;
+            while ((data = bufferedReader.readLine()) != null) {
+                strBuilder.append(data).append("\n");
+            }
+            Log.d(TAG, "run: "+strBuilder.toString());
+
+        }
+        catch (Exception e){
+            Log.d(TAG, "run: "+e.toString());
+        }
     }
 }
--- app/src/main/java/comsaleh/caloriesapp/MainActivity.java
+++ app/src/main/java/comsaleh/caloriesapp/MainActivity.java
@@ -17,10 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dataTxt =  findViewById(R.id.dataView);
+        searchTxt = findViewById(R.id.foodText);
     }
 
     public void search(View v){
 
+        String str = searchTxt.getText().toString().trim();
+        new Thread(new CaloriesRunnable(str,this)).start();
     }
 
 }
